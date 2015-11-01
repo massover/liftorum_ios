9e2b56913c4393db91dae3e45bc25f6102c5beaf
Voxy <voxy@massover.local>
2015-11-01 15:26:49 -0500

    First commit

--- liftorum/ViewController.swift
+++ liftorum/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weightTextField: UITextField!
+    
+    @IBOutlet weak var repsTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +23,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func submitLift(sender: UIButton) {
+        print(repsTextField?.text)
+        print(weightTextField?.text)
+    }
+    
 
 
 }
