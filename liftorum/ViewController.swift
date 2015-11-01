ad10d07d1455c7763cb9474126e208c0af8948a6
Voxy <voxy@client.openvpn.net>
2015-11-01 14:46:38 -0500

    Initial Commit

---
+++ liftorum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  liftorum
+//
+//  Created by Voxy on 11/1/15.
+//  Copyright © 2015 liftorum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
