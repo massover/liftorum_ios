7116eabb41e4528c44cb74c5ff0afeea63b629b5
Voxy <voxy@massover.local>
2015-11-07 19:47:35 -0500

    Add imageview connected to library

--- liftorum/ViewController.swift
+++ liftorum/ViewController.swift
@@ -9,13 +9,20 @@
 import UIKit
 import Alamofire
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class ViewController:
+    UIViewController,
+    UIPickerViewDataSource,
+    UIPickerViewDelegate,
+    UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate
+{
 
     var pickerDataSource = ["Squat", "Bench", "Deadlift"]
     
     @IBOutlet weak var weightTextField: UITextField!
     @IBOutlet weak var repsTextField: UITextField!
     @IBOutlet weak var liftPickerView: UIPickerView!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +31,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         self.liftPickerView.reloadAllComponents()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -41,6 +48,36 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return pickerDataSource[row]
     }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // The info dictionary contains multiple representations of the image, and this uses the original.
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Set photoImageView to display the selected image.
+        imageView.image = selectedImage
+        
+        // Dismiss the picker.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func selectVideoFromLibrary(sender: UITapGestureRecognizer) {
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .PhotoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        
+        presentViewController(imagePickerController, animated: true, completion: nil)
+    }
+    
     
     @IBAction func submitLift(sender: UIButton) {
         let row = liftPickerView.selectedRowInComponent(0)
@@ -67,7 +104,6 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         }
 
     }
-    
 
 
 }
