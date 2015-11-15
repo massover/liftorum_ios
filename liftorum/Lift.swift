3906ad574e394e7d13b68ac6a37d746c152f8af8
Voxy <voxy@massover.local>
2015-11-15 15:40:12 -0500

    Convert iso string from json to nsdate

--- liftorum/Lift.swift
+++ liftorum/Lift.swift
@@ -21,15 +21,26 @@ final class User: ResponseObjectSerializable{
 
 }
 
+func convertISOStringToNSDate(ISOString: String) -> NSDate{
+    let dateFormatter = NSDateFormatter()
+    dateFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS"
+    return dateFormatter.dateFromString(ISOString)!
+}
+
 final class Lift : ResponseObjectSerializable{
     let createdAt: NSDate
     let id: Int
     let user: User
     
     init?(response: NSHTTPURLResponse, representation: AnyObject) {
-        self.createdAt = NSDate()
         self.id = representation.valueForKeyPath("id") as! Int
-        self.user = User(response:response, representation: representation.valueForKeyPath("user")!)!
+        let createdAt = representation.valueForKeyPath("created_at") as! String
+        self.createdAt = convertISOStringToNSDate(createdAt)
+        self.user = User(
+            response:response,
+            representation: representation.valueForKeyPath("user")!
+        )!
     }
         
     
