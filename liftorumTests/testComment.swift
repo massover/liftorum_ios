3743ca32e9ffd016531d30f44443aee02716b893
Voxy <voxy@massover.local>
2015-12-30 20:27:27 -0500

    Add test for comments

--- liftorumTests/testComment.swift
+++ liftorumTests/testComment.swift
@@ -2,11 +2,44 @@
 //  testComment.swift
 //  liftorum
 //
-//  Created by Voxy on 12/24/15.
+//  Created by Voxy on 12/30/15.
 //  Copyright © 2015 liftorum. All rights reserved.
 //
 
+import Alamofire
 import XCTest
+@testable import liftorum
+
+extension XCTestCase{
+    func createComment(text: String, liftId: Int, userId: Int) -> Comment{
+        let path = Router.CreateComment([:]).path
+        let expectation = expectationWithDescription("\(path) 201")
+        var comment: Comment!
+        
+        var _response: NSHTTPURLResponse?
+        let completionHandler = { (response:Response<Comment, NSError>) in
+            _response = response.response
+            if let value = response.result.value {
+                comment = value
+            }
+            expectation.fulfill()
+        }
+        
+        Comment.create(
+            text,
+            liftId: liftId,
+            userId: userId,
+            completionHandler: completionHandler
+        )
+        
+        waitForExpectationsWithTimeout(5, handler: nil)
+        XCTAssertEqual(_response?.statusCode, 201)
+        XCTAssertEqual(comment.text, text)
+        return comment
+        
+    }
+    
+}
 
 class testComment: XCTestCase {
 
@@ -20,19 +53,20 @@ class testComment: XCTestCase {
         super.tearDown()
     }
 
-    func testCreateComment() {
-        Video.create(
-            
+    func testExample() {
+        self.setUpDatabase()
+        var user: User
+        var video: Video
+        var lift: Lift
+        
+        user = self.createUser()
+        video = self.createVideo()
+        lift = self.createLift(user.id!, videoId: video.id!)
+        self.createComment(
+            "Some text for a comment",
+            liftId: lift.id!,
+            userId: user.id!
         )
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
